eb8332100607f1eac2e0ebde0f0e4ad29e70af4b
Sasha Ivanov <Sasha@sashas-macbook-pro.local>
2016-10-09 15:56:31 -0600

    Initial Commit

---
+++ MIKMIDI-iOS-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MIKMIDI-iOS-Example
+//
+//  Created by Sasha Ivanov on 2016-10-09.
+//  Copyright © 2016 madebysasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
